674dc15bde36bd7ac1cf57409c015615ba3ac762
Wellington <wjas.ma@gmail.com>
2023-06-28 00:29:37 -0300

    Board page :: VIP conteiner element added

--- src/app/board/page.tsx
+++ src/app/board/page.tsx
@@ -1,63 +1,81 @@
-import styles from './bord.module.scss'
-import { FiPlus, FiCalendar, FiTrash, FiEdit } from 'react-icons/fi'
+import styles from './board.module.scss'
+import { 
+  FiPlus, 
+  FiCalendar, 
+  FiTrash, 
+  FiEdit,
+  FiClock 
+} from 'react-icons/fi'
 export default function Board() {
   return (
-    <div className={styles.container}>
-      <form action="">
-        <input
-          id='task'
-          type='text'
-          placeholder='Digite sua tarefa'
-        />
+    <>
+      <div className={styles.container}>
+        <form action="">
+          <input
+            id='task'
+            type='text'
+            placeholder='Digite sua tarefa'
+          />
 
-        <button type='submit'>
-          <FiPlus size={30} />
-        </button>
-      </form>
+          <button type='submit'>
+            <FiPlus size={30} />
+          </button>
+        </form>
 
-      <h2>Você tem 2 tarefas</h2>
+        <h2>Você tem 2 tarefas</h2>
 
-      <section>
-      <article className={styles.taskList}>
-          <p>Aprender a criar projetos usanto NextJs aplicando firebase como back.</p>
-          <div className={styles.actions}>
-            <div className={styles.taskInfo}>
-              <div>
-                <time>
-                  <FiCalendar size={20} color='ffb800' />
-                  25 de Julho de 2023
-                </time>
+        <section>
+          <article className={styles.taskList}>
+            <p>Aprender a criar projetos usanto NextJs aplicando firebase como back.</p>
+            <div className={styles.actions}>
+              <div className={styles.taskInfo}>
+                <div>
+                  <time>
+                    <FiCalendar size={20} color='ffb800' />
+                    25 de Julho de 2023
+                  </time>
+                </div>
+                <button className={styles.edit}>
+                  <FiEdit size={20} />
+                </button>
               </div>
-              <button className={styles.edit}>
-                <FiEdit size={20}/>
+              <button className={styles.delete}>
+                <FiTrash size={20} />
               </button>
             </div>
-            <button className={styles.delete}>
-              <FiTrash size={20} />
-            </button>
-          </div>
-        </article>
+          </article>
 
-        <article className={styles.taskList}>
-          <p>Estudar ReactJs :: Styled Components e React Router DOM.</p>
-          <div className={styles.actions}>
-            <div className={styles.taskInfo}>
-              <div>
-                <time>
-                  <FiCalendar size={20} color='ffb800' />
-                  25 de Julho de 2023
-                </time>
+          <article className={styles.taskList}>
+            <p>Estudar ReactJs :: Styled Components e React Router DOM.</p>
+            <div className={styles.actions}>
+              <div className={styles.taskInfo}>
+                <div>
+                  <time>
+                    <FiCalendar size={20} color='ffb800' />
+                    25 de Julho de 2023
+                  </time>
+                </div>
+                <button className={styles.edit}>
+                  <FiEdit size={20} />
+                </button>
               </div>
-              <button className={styles.edit}>
-                <FiEdit size={20}/>
+              <button className={styles.delete}>
+                <FiTrash size={20} />
               </button>
             </div>
-            <button className={styles.delete}>
-              <FiTrash size={20} />
-            </button>
-          </div>
-        </article>
-      </section>
-    </div>
+          </article>
+        </section>
+      </div>
+
+      <div className={styles.vipContainer}>
+        <h3>Obrigado por apoiar esse projeto.</h3>
+        <div className={styles.timer}>
+          <FiClock size={28}/>
+          <time>
+            Ultima doação foi a 3 dias.
+          </time>
+        </div>
+      </div>
+    </>
   )
 }
