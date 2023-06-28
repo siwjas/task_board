44d5922c3f2b3a113cc75e18616ecef925509ba0
Wellington <wjas.ma@gmail.com>
2023-06-28 00:08:47 -0300

    Home page content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,25 @@
 import styles from './home.module.scss'
+import Image from 'next/image'
+import { FiCheckCircle } from 'react-icons/fi'
+import boardUser from '../../public/board-user.svg'
+import Sponsors from '@/components/Sponsors'
 
 export default function Home() {
+  
   return (
     <main className={styles.container}>
-      <div>
-       <h1 className={styles.title}>Phojeto em <span>NextJS</span></h1>
-       <p>A nice App for organize your tasks!</p>
-      </div>
+      <Image src={boardUser} alt='Board user' width={400} />
+      <section>
+        <h1 className={styles.title}>
+          Uma ferramenta para seu dia a dia. <br />
+          Escreva, planeje e <span>organise-se <FiCheckCircle size={24} /> </span>
+        </h1>
+        <h3><span>100% Gratuito</span> e online!</h3>
+      </section>
+
+      <section className={styles.donators}>
+        <Sponsors isDonator = {true} />
+      </section>
     </main>
   )
 }
