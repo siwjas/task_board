94db50adc7ca9c0931bb37c57b2f5173a4fd4a39
Wellington <wjas.ma@gmail.com>
2023-06-29 01:27:59 -0300

    Adjust page styles

--- src/app/board/page.tsx
+++ src/app/board/page.tsx
@@ -1,14 +1,14 @@
 import { SponsorButton } from '@/components/SponsorButton'
 import styles from './board.module.scss'
-import { 
-  FiPlus, 
-  FiCalendar, 
-  FiTrash, 
+import {
+  FiPlus,
+  FiCalendar,
+  FiTrash,
   FiEdit,
-  FiClock 
+  FiClock
 } from 'react-icons/fi'
 import { Metadata } from 'next'
- 
+
 export const metadata: Metadata = {
   title: 'Cadastre suas tarefas',
   description: 'Organize-se com essa ferramenta fantástica.'
@@ -16,79 +16,81 @@ export const metadata: Metadata = {
 
 export default function Board() {
   return (
-    <>
-      <div className={styles.container}>
-        <form action="">
-          <input
-            id='task'
-            type='text'
-            placeholder='Digite sua tarefa'
-          />
+    <div className={styles.container}>
+      <span className={styles.x}>.</span>
+      <div className={styles.content}>
+        <div className={styles.contentForm}>
+          <form action="">
+            <input
+              id='task'
+              type='text'
+              placeholder='Digite sua tarefa'
+            />
 
-          <button type='submit'>
-            <FiPlus size={30} />
-          </button>
-        </form>
+            <button type='submit'>
+              <FiPlus size={30} />
+            </button>
+          </form>
 
-        <h2>Você tem 2 tarefas</h2>
+          <h2>Você tem 2 tarefas</h2>
 
-        <section>
-          <article className={styles.taskList}>
-            <span className={styles.taskItem}>
-              <p>Aprender a criar projetos usanto NextJs aplicando firebase como back.</p>
-            </span>
-            
-            <div className={styles.actions}>
-              <div className={styles.taskInfo}>
-                <div>
-                  <time>
-                    <FiCalendar size={20} color='ffb800' />
-                    25 de Julho de 2023
-                  </time>
+          <section>
+            <article className={styles.taskList}>
+              <span className={styles.taskItem}>
+                <p>Aprender a criar projetos usanto NextJs aplicando firebase como back.</p>
+              </span>
+
+              <div className={styles.actions}>
+                <div className={styles.taskInfo}>
+                  <div>
+                    <time>
+                      <FiCalendar size={20} color='ffb800' />
+                      25 de Julho de 2023
+                    </time>
+                  </div>
+                  <button className={styles.edit}>
+                    <FiEdit size={20} />
+                  </button>
                 </div>
-                <button className={styles.edit}>
-                  <FiEdit size={20} />
+                <button className={styles.delete}>
+                  <FiTrash size={20} />
                 </button>
               </div>
-              <button className={styles.delete}>
-                <FiTrash size={20} />
-              </button>
-            </div>
-          </article>
+            </article>
 
-          <article className={styles.taskList}>
-            <p>Estudar ReactJs :: Styled Components e React Router DOM.</p>
-            <div className={styles.actions}>
-              <div className={styles.taskInfo}>
-                <div>
-                  <time>
-                    <FiCalendar size={20} color='ffb800' />
-                    25 de Julho de 2023
-                  </time>
+            <article className={styles.taskList}>
+              <p>Estudar ReactJs :: Styled Components e React Router DOM.</p>
+              <div className={styles.actions}>
+                <div className={styles.taskInfo}>
+                  <div>
+                    <time>
+                      <FiCalendar size={20} color='ffb800' />
+                      25 de Julho de 2023
+                    </time>
+                  </div>
+                  <button className={styles.edit}>
+                    <FiEdit size={20} />
+                  </button>
                 </div>
-                <button className={styles.edit}>
-                  <FiEdit size={20} />
+                <button className={styles.delete}>
+                  <FiTrash size={20} />
                 </button>
               </div>
-              <button className={styles.delete}>
-                <FiTrash size={20} />
-              </button>
-            </div>
-          </article>
-        </section>
-      </div>
-
-      <div className={styles.vipContainer}>
-        <h3>Obrigado por apoiar esse projeto.</h3>
-        <div className={styles.timer}>
-          <FiClock size={28}/>
-          <time>
-            Ultima doação foi a 3 dias.
-          </time>
+            </article>
+          </section>
         </div>
+
+        <div className={styles.vipContainer}>
+          <h3>Obrigado por apoiar esse projeto.</h3>
+          <div className={styles.timer}>
+            <FiClock size={28} />
+            <time>
+              Ultima doação foi a 3 dias.
+            </time>
+          </div>
+        </div>
+        <SponsorButton isDonator={false} />
       </div>
-      
-      <SponsorButton isDonator={false} />
-    </>
+    </div>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,21 +5,23 @@ import boardUser from './assets/board-user.svg'
 import Sponsors from '../components/Sponsors'
 
 export default function Home() {
-  
+
   return (
     <main className={styles.container}>
-      <Image src={boardUser} priority={true} alt='Board user' width={400} />
-      <section>
-        <h1 className={styles.title}>
-          Uma ferramenta para seu dia a dia. <br />
-          Escreva, planeje e <span>organise-se <FiCheckCircle size={24} /> </span>
-        </h1>
-        <h3><span>100% Gratuito</span> e online!</h3>
-      </section>
+      <div className={styles.content}>
+        <Image src={boardUser} priority={true} alt='Board user' width={400} />
+        <section>
+          <h1 className={styles.title}>
+            Uma ferramenta para seu dia a dia. <br />
+            Escreva, planeje e <span>organise-se <FiCheckCircle size={24} /> </span>
+          </h1>
+          <h3><span>100% Gratuito</span> e online!</h3>
+        </section>
 
-      <section className={styles.donators}>
-        <Sponsors isDonator = {true} />
-      </section>
+        <section className={styles.donators}>
+          <Sponsors isDonator={true} />
+        </section>
+      </div>
     </main>
   )
 }
